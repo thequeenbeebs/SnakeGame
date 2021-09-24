a125f9d08a1736477c301a23055feadddb454dd4
Blaire Baker <bakerblaire@gmail.com>
2021-09-23 21:20:33 -0600

    moved snake class to separate file, created movement abilities

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
 # Planning
@@ -15,26 +16,20 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('My Snake Game')
 screen.tracer(0)
-x_coordinates = [0, -20, -40]
 
-segments = []
+snake = Snake()
 
-for index in range(3):
-    square = Turtle('square')
-    square.color('white')
-    square.penup()
-    square.goto(x_coordinates[index], 0)
-    segments.append(square)
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
